b27dfe5b5fd8df5a641e7a39764dd18070766ae7
Cemal Taşkın <77429146+CemalTkn@users.noreply.github.com>
2023-10-24 09:42:36 +0300

    byte veri tipi ekleme

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -14,9 +14,11 @@ namespace TypesAndVariables
             int number1 = 2147483647;
             long number2 = 9223372036854775807;
             short number3 = 32767;
+            byte number4 = 255;
             Console.WriteLine("Number1 is {0}", number1);
             Console.WriteLine("Number2 is {0}", number2);
             Console.WriteLine("Number3 is {0}", number3);
+            Console.WriteLine("Number4 is {0}", number4);
             Console.ReadLine();
         }
     }

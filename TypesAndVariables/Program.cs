f6d997e9161ae80374b2110e878fdd793df70ea7
Cemal Taşkın <77429146+CemalTkn@users.noreply.github.com>
2023-10-24 09:56:35 +0300

    enum veri tipi tanımlama ve kullanımı

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -19,6 +19,7 @@ namespace TypesAndVariables
             char character = 'A';
             double number5 = 10.4;
             decimal number6 = 10.4M;
+
             Console.WriteLine("Number1 is {0}", number1);
             Console.WriteLine("Number2 is {0}", number2);
             Console.WriteLine("Number3 is {0}", number3);
@@ -26,7 +27,13 @@ namespace TypesAndVariables
             Console.WriteLine("Character is : {0}",(int) character);
             Console.WriteLine("Number5 is {0}", number5);
             Console.WriteLine("Number6 is {0}", number6);
+            Console.WriteLine((int)Days.Friday);
             Console.ReadLine();
         }
+    }
+
+    enum Days
+    {
+        Monday=10, Tuesday=20, Wednesday=30, Thursday, Friday, Saturday, Sunday //Başlangıç değeri verebiliriz.
     }
 }

68cd6deccfb1579a37d816421908bc6c733da96e
Cemal Taşkın <77429146+CemalTkn@users.noreply.github.com>
2023-10-24 14:49:39 +0300

    do while döngüsü ile çalışmak

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -10,15 +10,28 @@ namespace Loops
     {
         static void Main(string[] args)
         {
-            ForLoop();
+            //ForLoop();
+            //WhileLoop();
+            int number = 10;
+            do
+            {
+                Console.WriteLine(number);
+                number--;
+
+            } while (number>=11);
+            
+            Console.ReadLine();
+        }
+
+        private static void WhileLoop()
+        {
             int number = 100;
-            while (number>=0)
+            while (number >= 0)
             {
                 Console.WriteLine(number);
                 number--;
             }
-            Console.WriteLine("Now number is {0}",number);
-            Console.ReadLine();
+            Console.WriteLine("Now number is {0}", number);
         }
 
         private static void ForLoop()

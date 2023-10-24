d8383d65cd641ae51d850c82a9be523726c0de52
Cemal Taşkın <77429146+CemalTkn@users.noreply.github.com>
2023-10-24 10:24:08 +0300

    iç içe if ile çalışma

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -56,19 +56,26 @@ namespace Conditionals
             //}
             //Console.ReadLine();
 
-            if (number >= 0 && number <= 100)
+            //if (number >= 0 && number <= 100)
+            //{
+            //    Console.WriteLine("Number is between 0-100");
+            //}
+            //else if (number > 100 && number <= 200)
+            //{
+            //    Console.WriteLine("Number is between 101-200");
+            //}
+            //else if (number > 200 || number < 0)
+            //{
+            //    Console.WriteLine("Number is less than 0 or greater than 200");
+            //}
+
+            if (number<100)
             {
-                Console.WriteLine("Number is between 0-100");
+                if (number>=90&&number<95)
+                {
+
+                }
             }
-            else if (number > 100 && number <= 200)
-            {
-                Console.WriteLine("Number is between 101-200");
-            }
-            else if (number > 200 || number < 0)
-            {
-                Console.WriteLine("Number is less than 0 or greater than 200");
-            }
-            
             Console.ReadLine();
         }
     }

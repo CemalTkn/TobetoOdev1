d25ad556c8d2ab9054f4b0b1a826ca282c58f829
Cemal Taşkın <77429146+CemalTkn@users.noreply.github.com>
2023-10-24 14:25:29 +0300

    çok boyutlu diziler ile çalışmak

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -18,13 +18,31 @@ namespace Arrays
             string[] students3 = new string[3] { "Engin", "Cemal", "İdil" };
 
             string[] students2 = { "Engin", "Cemal", "İdil" };
-            students2[3] = "Ahmet";//Çalışmaz
-           
+            //students2[3] = "Ahmet";//Çalışmaz
 
-            foreach (var student in students) 
+            //foreach (var student in students) 
+            //{
+            //    Console.WriteLine(student);
+            //}
+
+            string[,] regions = new string[5, 3]
             {
-                Console.WriteLine(student);
+                {"İstanbul","İzmit","Balıkesir"},
+                {"Ankara","Konya","Kırıkkale"},
+                {"Antalya","Adana","Mersin"},
+                {"Rize","Trabzon","Samsun"},
+                {"İzmir","Muğla","Manisa"}
+            };
+
+            for (int i = 0; i <= regions.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= regions.GetUpperBound(1); j++)
+                {
+                    Console.WriteLine(regions[i,j]);
+                }
+                Console.WriteLine("***********");
             }
+
             Console.WriteLine();
             Console.ReadLine();
         }

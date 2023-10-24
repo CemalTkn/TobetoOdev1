4672b258fe18e57e79342de34c74bb39f560f249
Cemal Taşkın <77429146+CemalTkn@users.noreply.github.com>
2023-10-24 09:38:03 +0300

    long veri tipi ekleme

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -11,8 +11,10 @@ namespace TypesAndVariables
         static void Main(string[] args)
         {
             //Value Types
-            int number1 = 10;
+            int number1 = 2147483647;
+            long number2 = 9223372036854775807;
             Console.WriteLine("Number1 is {0}", number1);
+            Console.WriteLine("Number2 is {0}", number2);
             Console.ReadLine();
         }
     }

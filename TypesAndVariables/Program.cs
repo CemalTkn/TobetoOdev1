9c8a2423bc7239c20a63436d91cffc796eea7f88
Cemal Taşkın <77429146+CemalTkn@users.noreply.github.com>
2023-10-24 09:40:42 +0300

    short veri tipi ekleme

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -13,8 +13,10 @@ namespace TypesAndVariables
             //Value Types
             int number1 = 2147483647;
             long number2 = 9223372036854775807;
+            short number3 = 32767;
             Console.WriteLine("Number1 is {0}", number1);
             Console.WriteLine("Number2 is {0}", number2);
+            Console.WriteLine("Number3 is {0}", number3);
             Console.ReadLine();
         }
     }

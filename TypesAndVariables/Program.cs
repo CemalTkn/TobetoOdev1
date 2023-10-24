5ac9dced46e686e18e7c39940a8002b5c9c947d1
Cemal Taşkın <77429146+CemalTkn@users.noreply.github.com>
2023-10-24 09:49:15 +0300

    double veri tipi tanımlama

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -17,11 +17,13 @@ namespace TypesAndVariables
             byte number4 = 255;
             bool condition = true;
             char character = 'A';
+            double number5 = 10.4;
             Console.WriteLine("Number1 is {0}", number1);
             Console.WriteLine("Number2 is {0}", number2);
             Console.WriteLine("Number3 is {0}", number3);
             Console.WriteLine("Number4 is {0}", number4);
             Console.WriteLine("Character is : {0}",(int) character);
+            Console.WriteLine("Number5 is {0}", number5);
             Console.ReadLine();
         }
     }

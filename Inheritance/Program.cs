ebccc411134a25cfc551b60a322e6a3160f36b5e
Cemal Taşkın <77429146+CemalTkn@users.noreply.github.com>
2023-10-26 15:14:32 +0300

    Kalıtıma giriş

---
+++ Inheritance/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[3]
+            {
+                new Customer
+                {FirstName="Cemal"},
+                new Student
+                {FirstName="Engin"},
+                new Person
+                {FirstName="İdil"}
+            };
+            
+            foreach (Person person in persons)
+            {
+                Console.WriteLine(person);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    class Customer : Person
+    {
+        public string City { get; set; }
+    }
+
+    class Student : Person
+    {
+        public string Department { get; set; }
+    }
+}

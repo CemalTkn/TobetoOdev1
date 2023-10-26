35e02473d21908040f10f82998b0c31fc4855d88
Cemal Taşkın <77429146+CemalTkn@users.noreply.github.com>
2023-10-26 09:44:24 +0300

    Classlar içinde property tanımlamak.

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -18,6 +18,19 @@ namespace Classes
             productManager.Add();
             productManager.Update();
 
+            Customer customer = new Customer();
+            customer.City = "Ankara";
+            customer.Id = 1;
+            customer.FirstName= "Engin";
+            customer.LastName = "Demiroğ";
+
+            Customer customer2 = new Customer
+            {
+                Id = 2, City = "İstanbul",
+                FirstName = "Cemal", LastName= "Taşkın"
+            };
+            Console.WriteLine(customer2.FirstName);
+
             Console.ReadLine();
         }
     }

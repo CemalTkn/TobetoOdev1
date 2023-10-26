15d9579739a4a07d597380837af8998e706e9c07
Cemal Taşkın <77429146+CemalTkn@users.noreply.github.com>
2023-10-26 11:04:43 +0300

    Interface kullanımı 2

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,21 +11,42 @@ namespace Interfaces
     {
         static void Main(string[] args)
         {
+            PersonManager manager = new PersonManager();
+
+            Customer customer =  new Customer
+            {   Id = 1,
+                FirstName = "Cemal",
+                LastName = "Taşkın",
+                Address = "İstanbul"
+            };          
+
+            Student student = new Student
+            {
+                Id = 1,
+                FirstName = "İdil",
+                LastName = "Taşkın",
+                Department = "Computer Science"
+            };   
+
+            manager.Add(student);
+            manager.Add(customer);            
+
+            Console.ReadLine();
         }
     }
 
     interface IPerson 
     {
         int Id { get; set; }
-        int FirstName { get; set; }
-        int LastName { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
     }
 
     class Customer : IPerson
     {
         public int Id { get; set; }
-        public int FirstName { get; set; }
-        public int LastName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
 
         public string Address { get; set; }
     }
@@ -32,9 +54,26 @@ namespace Interfaces
     class Student : IPerson
     {
         public int Id { get; set; }
-        public int FirstName { get; set; }
-        public int LastName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
 
-        public string Address { get; set; }
+        public string Department { get; set; }
+    }
+
+    class Worker : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Department { get; set; }
+    }
+
+    class PersonManager
+    {
+        public void Add(IPerson person)
+        {
+            Console.WriteLine(person.FirstName);
+        }
     }
 }
